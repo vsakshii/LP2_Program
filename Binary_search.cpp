1036f3090db888972d6f4005bbe1bcfed3a5b5fd
vsakshii <121532111+vsakshii@users.noreply.github.com>
2023-03-17 13:22:03 +0530

    Update Binary_search.cpp

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -1,34 +1,42 @@
-// Binary Search in C++
-
 #include <iostream>
 using namespace std;
 
-int binarySearch(int array[], int x, int low, int high) {
-  
-	// Repeat until the pointers low and high meet each other
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
-
-    if (array[mid] == x)
-      return mid;
-
-    if (array[mid] < x)
-      low = mid + 1;
-
+int main()
+{
+    int n;
+    cout<<"Enter the size of array =>";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter "<<n<<" Integers in ascending order => \n";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];    
+    
+    int target;
+    cout<<"Enter the number you want to search => ";
+    cin>>target;
+    
+    int start = 0, end = n-1, mid;
+    int pos= -1;
+    
+    while(start<=end)
+    {
+        mid = (start+end)/2;
+        
+        if(arr[mid]==target){
+            pos = mid;
+            break;
+        }
+        else if(arr[mid]<target)
+            end = mid -1;   
+        else
+            start = mid + 1;
+    }
+    
+    if(pos==-1)
+        cout<<target<<" is not present in the array"<<endl;
     else
-      high = mid - 1;
-  }
-
-  return -1;
-}
-
-int main(void) {
-  int array[] = {3, 4, 5, 6, 7, 8, 9};
-  int x = 4;
-  int n = sizeof(array) / sizeof(array[0]);
-  int result = binarySearch(array, x, 0, n - 1);
-  if (result == -1)
-    printf("Not found");
-  else
-    printf("Element is found at index %d", result);
+        cout<<target<<" is present in the array at position "<<pos<<endl;
+        
+    return 0;
 }
